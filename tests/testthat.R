c90d0de229ac071d811050ee5298ca24e866640a
Svenja Jedhoff <sjedhoff@gmx.de>
2024-08-23 12:48:07 +0200

    update testthat file

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(myfirstpackage)
+library(CovCorTest)
 
-test_check("myfirstpackage")
+test_check("CovCorTest")
